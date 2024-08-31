482e251f3736f1be81840c6cace67a17a3c68471
dagon12345 <lanceespina478@gmail.com>
2024-08-31 22:14:26 +0800

    Problem number 1 unit 5 of 8 solved

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,26 +1,61 @@
-﻿/*
-    In this code you reverse the string 
-    originalMessage using Array.Reverse class and method function
-    and it will count the letter 'o' present in the originalMessage
+﻿/*Fizz Buzz code
+for (int i = 1; i <= 100; i++)
+{
+    Console.Write(i);
+    Console.WriteLine(i % 3 == 0 && i % 5 == 0 ? " - FizzBuzz" : i % 3 == 0 ? " - Fizz": i % 5 == 0 ? " - Buzz" : "");
+}
+*/
+//Monster game below.
+/*
+Random random = new Random();
+int MonsterHealth = 10;
+int HeroHealth = 10;
+
+do{
+    int roll = random.Next(1, 11);
+    MonsterHealth -= roll;
+    Console.WriteLine($"Monster was damage and lost {roll} health and now has {MonsterHealth} health.");
+
+    if(MonsterHealth <= 0) continue; //continue means break or done if true.
+    //IF above is true then trigger the below code which is the while then write it in the Console.WriteLine.
+
+    roll = random.Next(1, 11);
+    HeroHealth -= roll;
+    Console.WriteLine($"Hero was damage and lost {roll} health and now has {HeroHealth} health.");
+
+}while(HeroHealth > 0 && MonsterHealth > 0);
+Console.WriteLine(HeroHealth > MonsterHealth ? "Hero wins!" : "Monster wins!");
 */
 
-
-string originalMessage = "The quick brown fox jumps over the lazy dog.";
-
-char[] charMessage = originalMessage.ToCharArray();
-Array.Reverse(charMessage);
-
-int letterCount = 0;
-
-foreach(char letter in charMessage)
-{
-    if(letter == 'o')
-    {
-        letterCount++;
-    }
+//Problem number one solved.
+string? readResult;
+int numericValue = 0;
+bool validNumber = false;
+Console.WriteLine("Please enter an integer value between 5 and 10");
+do{
      
-}
-string newMessage = new string(charMessage);
+    readResult = Console.ReadLine();
+    //validNumber = int.TryParse(readResult, out numericValue); 
+    if(readResult != null)
+    {
+        validNumber = int.TryParse(readResult, out numericValue);
+        if(validNumber && numericValue >= 5 && validNumber && numericValue <=10)
+        {
+            validNumber = true;
+            Console.WriteLine($"Your input value {readResult} has been accepted.");
 
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {letterCount} times.");
+        }
+        else if( validNumber && numericValue <=5 || validNumber && numericValue >= 10)
+        {
+             Console.WriteLine($"You entered {readResult}, please enter a number between 5 and 10");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you've entered invalid number.");
+
+        }
+
+    }
+
+}while(validNumber == false);
+//validNumber = int.TryParse(readResult, out numericValue);
