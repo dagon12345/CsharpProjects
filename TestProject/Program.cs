ed7233f9c6b69a899e16d943bff2924524f47668
dagon12345 <lanceespina478@gmail.com>
2024-09-02 22:01:33 +0800

    To be studied tomorrow

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -27,7 +27,7 @@ do{
 Console.WriteLine(HeroHealth > MonsterHealth ? "Hero wins!" : "Monster wins!");
 */
 
-//Problem number one solved.
+/*Problem number one solved.
 string? readResult;
 int numericValue = 0;
 bool validNumber = false;
@@ -51,11 +51,85 @@ do{
         }
         else
         {
-            Console.WriteLine("Sorry you've entered invalid number.");
+            Console.WriteLine("Sorry you've entered invalid number, please try again.");
 
         }
 
     }
 
 }while(validNumber == false);
-//validNumber = int.TryParse(readResult, out numericValue);
+
+*/
+/*
+string? readResult;
+string roleName = "";
+bool validEntry = false;
+
+do
+{                
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        roleName = readResult.Trim();
+    }
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+readResult = Console.ReadLine();
+
+*/
+
+// string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+// int stringsCount = myStrings.Length;
+
+// string myString = "";
+// int periodLocation = 0;
+
+// for (int i = 0; i < stringsCount; i++)
+// {
+//     myString = myStrings[i];
+//     periodLocation = myString.IndexOf(".");
+
+//     string mySentence;
+
+//     // extract sentences from each string and display them one at a time
+//     while (periodLocation != -1)
+//     {
+
+//         // first sentence is the string value to the left of the period location
+//         mySentence = myString.Remove(periodLocation);
+
+//          // the remainder of myString is the string value to the right of the location
+//          myString = myString.Substring(periodLocation + 1);
+
+//         // // remove any leading white-space from myString
+//          myString = myString.TrimStart();
+
+//         // // update the comma location and increment the counter
+//          periodLocation = myString.IndexOf(".");
+
+//          Console.WriteLine(mySentence);
+//     }
+ 
+//     mySentence = myString.Trim();
+//     Console.WriteLine(mySentence);
+// }
+
+
+// for(int i = 1; i < 100; i++)
+// {
+//     Console.Write(i);
+//     Console.WriteLine(i % 3 == 0 && i % 5 == 0 ? "- FizzBuzz": i % 3 == 0 ? " - Fizz": i % 5 == 0 ? "- Buzz": "");
+   
+// }
